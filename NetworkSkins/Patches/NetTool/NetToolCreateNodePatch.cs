c118b9b78a069ad94951d56fc601e984662da9bf
boformer <felix.free@gmx.de>
2019-09-15 20:55:09 +0300

    Allow upgrades to the same NetInfo if the skin is different. Closes #34

--- NetworkSkins/Patches/NetTool/NetToolCreateNodePatch.cs
+++ NetworkSkins/Patches/NetTool/NetToolCreateNodePatch.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Reflection;
 using System.Reflection.Emit;
 using Harmony;
+using NetworkSkins.Net;
+using NetworkSkins.Skins;
 using UnityEngine;
 // ReSharper disable InconsistentNaming
 
 namespace NetworkSkins.Patches.NetTool
 {
+    /// <summary>
+    /// Changes:
+    /// * Display selected pillar while placing road, 
+    /// * Enable upgrading to the same kind of road (but with a different skin)
+    /// </summary>
     [HarmonyPatch]
     public static class NetToolCreateNodePatch
     {
@@ -45,8 +52,10 @@ namespace NetworkSkins.Patches.NetTool
             var originalInstructions = new List<CodeInstruction>(instructions);
 
             var netAiGetNodeBuildingMethod = typeof(NetAI).GetMethod("GetNodeBuilding");
+            var netAiCheckBuildPositionMethod = typeof(NetAI).GetMethod("CheckBuildPosition");
 
             var pillarPatcherGetActiveNodeBuildingMethod = typeof(ActivePillarPatcher).GetMethod("GetActiveNodeBuilding");
+            var patchCheckBuildPositionMethod = typeof(NetToolCreateNodePatch).GetMethod("CheckBuildPosition");
 
             if (netAiGetNodeBuildingMethod == null || pillarPatcherGetActiveNodeBuildingMethod == null)
             {
@@ -73,7 +82,53 @@ namespace NetworkSkins.Patches.NetTool
                 }
             }
 
+            // Replace all CheckBuildPosition calls with Patcher.CheckBuildPosition
+            for (var index = 0; index < codes.Count; index++)
+            {
+                if (codes[index].opcode == OpCodes.Callvirt)
+                {
+                    if (codes[index].operand == netAiCheckBuildPositionMethod)
+                    {
+                        TranspilerUtils.LogDebug("Found CheckBuildPosition(bool test, bool visualize, bool overlay, bool autofix, ref NetTool.ControlPoint startPoint, ref NetTool.ControlPoint middlePoint, ref NetTool.ControlPoint endPoint, out BuildingInfo ownerBuilding, out Vector3 ownerPosition, out Vector3 ownerDirection, out int productionRate)");
+                        codes[index] = new CodeInstruction(codes[index])
+                        {
+                            opcode = OpCodes.Call,
+                            operand = patchCheckBuildPositionMethod
+                        };
+                    }
+                }
+            }
+
             return codes;
+        }
+
+        // Patch for NetAI.CheckBuildPosition that makes it possible to upgrade a segment to a NetInfo of the same type if the skin is different
+        public static ToolBase.ToolErrors CheckBuildPosition(NetAI netAi, bool test, bool visualize, bool overlay, bool autofix, ref global::NetTool.ControlPoint startPoint, ref global::NetTool.ControlPoint middlePoint, ref global::NetTool.ControlPoint endPoint, out BuildingInfo ownerBuilding, out Vector3 ownerPosition, out Vector3 ownerDirection, out int productionRate)
+        {
+            var toolErrors = netAi.CheckBuildPosition(test, visualize, overlay, autofix, ref startPoint, ref middlePoint, ref endPoint, out ownerBuilding, out ownerPosition, out ownerDirection, out productionRate);
+
+            ushort upgradedSegment = middlePoint.m_segment;
+            if (startPoint.m_segment == upgradedSegment || endPoint.m_segment == upgradedSegment)
+            {
+                upgradedSegment = 0;
+            }
+
+            if(upgradedSegment != 0 && (toolErrors & ToolBase.ToolErrors.CannotUpgrade) != 0)
+            {
+                NetInfo upgradedSegmentInfo = global::NetManager.instance.m_segments.m_buffer[upgradedSegment].Info;
+                if (!(upgradedSegmentInfo.m_netAI.IsUnderground() && !netAi.SupportUnderground()))
+                {
+                    // Check if skin is different. If so, allow upgrade!
+                    var activeSkin = NetworkSkinManager.instance.GetActiveSkin(upgradedSegmentInfo);
+                    var upgradedSegmentSkin = NetworkSkinManager.SegmentSkins[upgradedSegment];
+                    if(!Equals(activeSkin, upgradedSegmentSkin))
+                    {
+                        toolErrors &= ~ToolBase.ToolErrors.CannotUpgrade;
+                    }
+                }
+            }
+
+            return toolErrors;
         }
     }
 }
