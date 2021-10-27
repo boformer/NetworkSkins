52bbc67bf2348eca8dc038088dc1b9cb3b2b516e
kianzzarrin2 <kian.zarrin@gmail.com>
2021-10-27 03:30:42 +0300

    fix autofit

--- NetworkSkins/GUI/Custom/CustomPanel.cs
+++ NetworkSkins/GUI/Custom/CustomPanel.cs
@@ -1,5 +1,6 @@
 ﻿using NetworkSkins.API;
 using NetworkSkins.GUI.Abstraction;
+using UnityEngine;
 
 namespace NetworkSkins.GUI.Custom {
     public class CustomPanel : PanelBase {
@@ -8,12 +9,21 @@ namespace NetworkSkins.GUI.Custom {
             Implementation.BuildPanel(this);
             color = GUIColor;
             base.Build(panelType, layout);
-            autoFitChildrenHorizontally = true;
+            autoFitChildrenHorizontally = autoFitChildrenVertically = true;
+            eventFitChildren += OnAutoFit;
         }
 
         protected override void RefreshUI(NetInfo netInfo) {
             if(Implementation.Enabled)
                 Implementation.RefreshUI();
         }
+
+        /// <summary>
+        /// workaround: fix auto fit bug in CO code.
+        /// </summary>
+        private void OnAutoFit() {
+            size += new Vector2(Spacing, Spacing);
+        }
+
     }
 }
