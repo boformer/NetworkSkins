1e966c00b08d55a065f440e6616cb5a1c019a411
kianzzarrin <kian.zarrin@gmail.com>
2022-09-30 12:10:54 +0300

    fixed patch now working

--- NetworkSkins/Patches/_NetNode/Wires/NetNodeRenderPatch.cs
+++ NetworkSkins/Patches/_NetNode/Wires/NetNodeRenderPatch.cs
@@ -5,6 +5,7 @@ using System.Reflection.Emit;
 using System.Reflection;
 using System;
 using UnityEngine;
+using NetworkSkins.Net;
 
 namespace NetworkSkins.Patches._NetNode.Wires
 {
@@ -20,7 +21,8 @@ namespace NetworkSkins.Patches._NetNode.Wires
         static MethodInfo mGetSegment => typeof(NetNode).Method(nameof(NetNode.GetSegment));
 
         /// <param name="counterGetSegment">
-        /// will use the n-th previous call to GetSegment() to determine segmentID.
+        /// if set to 0, segmentID is auto-calculated (only for end ndoes and DC bend nodes)
+        /// if set to n > 0, will use the n-th previous call to GetSegment() to determine segmentID.
         /// </param>
         public static void PatchCheckFlags(
             List<CodeInstruction> codes, MethodBase method, int occuranceCheckFlags, int counterGetSegment)
@@ -33,12 +35,14 @@ namespace NetworkSkins.Patches._NetNode.Wires
                     startIndex: iCheckFlags, count: -1);
 
                 CodeInstruction ldNodeInfo = codes[iLdNodeInfo].Clone();
+                CodeInstruction ldNodeID = TranspilerUtils.GetLDArg(method, "nodeID");
                 CodeInstruction ldSegmentID = BuildSegmentLDLocFromPrevSTLoc(codes, iCheckFlags, counterGetSegment);
                 CodeInstruction ldSegmentID2 = BuildSegmentLDLocFromPrevSTLoc(codes, iCheckFlags, counterGetSegment - 1);
                 var callShouldRender = typeof(NetNodeRenderPatch).Method(nameof(ShouldRender));
 
                 var newCodes = new[]{
                     ldNodeInfo,
+                    ldNodeID,
                     ldSegmentID,
                     ldSegmentID2,
                     new CodeInstruction(OpCodes.Call, callShouldRender),
@@ -56,26 +60,52 @@ namespace NetworkSkins.Patches._NetNode.Wires
         public static CodeInstruction BuildSegmentLDLocFromPrevSTLoc(
             List<CodeInstruction> codes, int index, int counter = 1)
         {
+            if (counter <= 0)
+                return new CodeInstruction(OpCodes.Ldc_I4_0); // load 0u (calculate in the injection)
             index = codes.Search(c => c.Calls(mGetSegment), startIndex: index, count: counter * -1);
             index = codes.Search(c => c.IsStloc(), startIndex: index);
             return codes[index].BuildLdLocFromStLoc();
         }
 
-        public static bool ShouldRender(NetInfo.Node node, ushort segment1, ushort segment2)
+        public static bool ShouldRender(NetInfo.Node nodeInfo, ushort nodeID, ushort segmentID1, ushort segmentID2)
         {
             if (_electricityNetShader == null)
             {
                 _electricityNetShader = Shader.Find("Custom/Net/Electricity");
             }
 
-            if (node.m_material?.shader != _electricityNetShader)
+            if (nodeInfo.m_material?.shader != _electricityNetShader)
             {
                 return true;
             }
 
-            var segmentSkin1 = NetworkSkinManager.SegmentSkins[segment1];
-            var segmentSkin2 = NetworkSkinManager.SegmentSkins[segment2];
+            if (segmentID1 == 0) {
+                GetBendSegmentIDs(nodeID, out segmentID1, out segmentID2);
+            }
+            var segmentSkin1 = NetworkSkinManager.SegmentSkins[segmentID1];
+            var segmentSkin2 = NetworkSkinManager.SegmentSkins[segmentID2];
             return (segmentSkin1 == null || segmentSkin1.m_hasWires) && (segmentSkin2 == null || segmentSkin2.m_hasWires);
+        }
+
+        static void GetBendSegmentIDs(ushort nodeID, out ushort segmentID1, out ushort segmentID2) {
+            ref NetNode node = ref nodeID.ToNode();
+            segmentID1 = 0;
+            segmentID2 = 0;
+            int segmentIndex = 0;
+            for (; segmentIndex < 8; segmentIndex++) {
+                ushort segmentID = node.GetSegment(segmentIndex);
+                if (segmentID != 0) {
+                    segmentID1 = segmentID;
+                    break;
+                }
+            }
+            for (; segmentIndex < 8; segmentIndex++) {
+                ushort segmentID = node.GetSegment(segmentIndex);
+                if (segmentID != 0 && segmentID != segmentID1) {
+                    segmentID2 = segmentID;
+                    break;
+                }
+            }
         }
     }
 }
